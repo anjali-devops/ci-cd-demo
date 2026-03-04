13cc1eddb5ffe248e18569496df1ad51d93b2562
Anjali <anjalidamisetty192003@gmail.com>
2026-03-04 07:16:13 +0000

    trigger pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Version 2 - CI/CD Demo 🚀"
+    return "Version 4 - CI/CD Demo 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
